28751bd156dc1fe6f4c77f5ba108c1169a1daf1f
Rumit Rout <rumitr@gmail.com>
2021-03-23 14:58:22 +0530

    changed folder structure in src

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from "./App";
+import React from "react";
+import { render } from "react-dom";
+import App from "./views/App";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 render(<App />, root);
